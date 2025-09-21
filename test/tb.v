05700f78fa6ad3a8c5c1991ace2c69b647c4239b
Eric Wang(Shiheng Wang) <wangshiheng61@gmail.com>
2025-09-20 23:32:51 -0400

    fix: Rename top module to comply with tt_um_ prefix

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  UWASIC_Shiheng user_project (
+  tt_um_UWASIC_Shiheng user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
