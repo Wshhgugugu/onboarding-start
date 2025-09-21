1cf7f0944017049c11e6fdbaa9cc4f423336d016
Eric Wang(Shiheng Wang) <wangshiheng61@gmail.com>
2025-09-21 00:31:42 -0400

    fix: Rename top module to comply with tt_um_ prefix

---
+++ src/spi_peripheral.v
@@ -0,0 +1,18 @@
+`default_nettype none
+
+module spi_peripheral (
+    input wire clk,
+    input wire rst_n,
+
+    input wire nCS,
+    input wire SCLK,
+    input wire COPI,
+
+    output reg [7:0] en_reg_out_7_0,
+    output reg [7:0] en_reg_out_15_8,
+    output reg [7:0] en_reg_pwm_7_0,
+    output reg [7:0] en_reg_pwm_15_8,
+    output reg [7:0] pwm_duty_cycle
+);
+
+endmodule
