bb0d47aafccc499d1f915951fa6ce8b24be7382a
Eric Wang(Shiheng Wang) <wangshiheng61@gmail.com>
2026-03-17 00:53:07 -0400

    Setup project with PWM peripheral

--- src/project.v
+++ src/project.v
@@ -1,11 +1,4 @@
-/*
- * Copyright (c) 2024 Shiheng
- * SPDX-License-Identifier: Apache-2.0
- */
-
-`default_nettype none
-
-module tt_um_UWASIC_Shiheng (
+module UWASIC_Shiheng (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -14,6 +7,7 @@ module tt_um_UWASIC_Shiheng (
     input  wire       ena,      // always 1 when the design is powered, so you can ignore it
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
+    assign uio_oe = 8'hFF; 
 );
 
   wire [7:0] en_reg_out_7_0;
@@ -37,16 +31,18 @@ module tt_um_UWASIC_Shiheng (
         .pwm_duty_cycle(pwm_duty_cycle)
     );
 
-     pwm_peripheral pwm_peripheral_inst (
-        .clk(clk),
-        .rst_n(rst_n),
-        .en_reg_out({en_reg_out_15_8, en_reg_out_7_0}),
-        .en_reg_pwm({en_reg_pwm_15_8, en_reg_pwm_7_0}),
-        .pwm_duty_cycle(pwm_duty_cycle),
-        .pwm_out({uio_out, uo_out})
-    );
-    
-    wire _unused = &{ui_in[7:3], uio_in, ena};
-    wire _unused = &{ena, ui_in[7:3], uio_in, 1'b0};
+      // Instantiate the PWM module
+  pwm_peripheral pwm_peripheral_inst (
+    .clk(clk),
+    .rst_n(rst_n),
+    .en_reg_out_7_0(en_reg_out_7_0),
+    .en_reg_out_15_8(en_reg_out_15_8),
+    .en_reg_pwm_7_0(en_reg_pwm_7_0),
+    .en_reg_pwm_15_8(en_reg_pwm_15_8),
+    .pwm_duty_cycle(pwm_duty_cycle),
+    .out({uio_out, uo_out})
+  );
+  // Add uio_in and ui_in[7:3] to the list of unused signals:
+  wire _unused = &{ena, ui_in[7:3], uio_in, 1'b0};
 
 endmodule
